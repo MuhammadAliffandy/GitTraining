0ffcb9473c8bd617908d4118dc59b1ee6584666e
MuhammadAliffandy <aliffandyrealgo@gmail.com>
2026-06-04 14:21:45 +0700

    Initial Commit

---
+++ GitTraining/GitTrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitTrainingApp.swift
+//  GitTraining
+//
+//  Created by Muhammad Aliffandy on 04/06/26.
+//
+
+import SwiftUI
+
+@main
+struct GitTrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

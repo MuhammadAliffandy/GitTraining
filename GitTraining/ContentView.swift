27bb6f4cc3c47286a83d33a5b13e21e4e12c24bc
Rozz <raden.mas.teguh.ibnu@gmail.com>
2026-06-04 15:38:24 +0700

    ganti nama dan gamber

--- GitTraining/ContentView.swift
+++ GitTraining/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "car.fill")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, fandy")
+            Text("Hello, Rozzeh")
         }
         .padding()
     }
